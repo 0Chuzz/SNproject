fe9cb93867068195f39784dda935a4371668d6be
0Chuzz <chuzz.chuzz+github@gmail.com>
2014-09-19 17:37:45 +0200

    paper

--- serial_sender.py
+++ serial_sender.py
@@ -22,6 +22,13 @@ labimage = ImageTk.PhotoImage("RGB", (320,240))
 lab = Tkinter.Label(root, image=labimage)
 lab.pack()
 listimgs = askopenfilenames(parent=root)
+listimgss = []
+while listimgs:
+        listimgss.append(listimgs)
+        listimgs = askopenfilenames(parent=root)
+        
+
+
 try:
         listimgs = listimgs.split(" ")
 except:
@@ -45,50 +52,52 @@ colors = [
 
 class MyApp (threading.Thread):
         def run(self):
-                for imgpath in listimgs:
-                        print "opening", imgpath
-                        img = Image.open(imgpath)
-                        imgb = img.tobytes()
-                        print "sending", len(imgb), "bytes"
-                        serial.write(img.tobytes())
-                        print "sent", serial.read(16), "bytes";
-                        readedresponse = serial.read(1);
-                        while readedresponse != "$":
-                                sys.stdout.write(readedresponse)
+                for listimgs in listimgss:
+                        serial.write("%10d" % (len(listimgs),))
+                        for imgpath in listimgs:
+                                print "opening", imgpath
+                                img = Image.open(imgpath)
+                                imgb = img.tobytes()
+                                print "sending", len(imgb), "bytes"
+                                serial.write(img.tobytes())
+                                print "sent", serial.read(16), "bytes";
                                 readedresponse = serial.read(1);
-                        """ When exiting from this loop read:
-                        1. The number of objects tracked (i.e. the number of bbox that will be received
-                        2. All the bounding box
-                        3. Retrieve original image, then draw the bounding box on them and finally show
-                        """
-                        readedresponse = serial.read(1)
-                        boundingBoxes =  []
-                        print "just readed ", readedresponse
-                        while readedresponse != "$":
-                                ident = serial.read(3).strip();
-                                topX = int(serial.read(3))
-                                topY = int(serial.read(3))
-                                botX = int(serial.read(3))
-                                botY = int(serial.read(3))
-                                boundingBoxes.append((ident, topX, topY, botX, botY))
+                                while readedresponse != "$":
+                                        sys.stdout.write(readedresponse)
+                                        readedresponse = serial.read(1);
+                                """ When exiting from this loop read:
+                                1. The number of objects tracked (i.e. the number of bbox that will be received
+                                2. All the bounding box
+                                3. Retrieve original image, then draw the bounding box on them and finally show
+                                """
                                 readedresponse = serial.read(1)
+                                boundingBoxes =  []
+                                print "just readed ", readedresponse
+                                while readedresponse != "$":
+                                        ident = serial.read(3).strip();
+                                        topX = int(serial.read(3))
+                                        topY = int(serial.read(3))
+                                        botX = int(serial.read(3))
+                                        botY = int(serial.read(3))
+                                        boundingBoxes.append((ident, topX, topY, botX, botY))
+                                        readedresponse = serial.read(1)
+                                        
+                                #img = Image.open(imgpath.replace("foreground_mask", "original")).convert("RGB")
+                                img = img.convert("RGB")
+                                drw = ImageDraw.Draw(img)
                                 
-                        #img = Image.open(imgpath.replace("foreground_mask", "original")).convert("RGB")
-                        img = img.convert("RGB")
-                        drw = ImageDraw.Draw(img)
-                        
-                        for bbox in boundingBoxes:
-                                ident, topX, topY, botX, botY = bbox
-                                col = colors[int(ident)%len(colors)]
-                                drw.rectangle([(topX, topY), (botX, botY)], outline=col)
-                                drw.text(((topX + botX)/2, (topY+botY)/2), ident, fill=col)
-                                print bbox
+                                for bbox in boundingBoxes:
+                                        ident, topX, topY, botX, botY = bbox
+                                        col = colors[int(ident)%len(colors)]
+                                        drw.rectangle([(topX, topY), (botX, botY)], outline=col)
+                                        drw.text(((topX + botX)/2, (topY+botY)/2), ident, fill=col)
+                                        print bbox
 
-                        labimage.paste(img)
-                        #sleep(5)
+                                labimage.paste(img)
+                                #sleep(5)
 
-                sleep(5)
-                exit()
+                #sleep(5)
+                #exit()
 
 thr = MyApp()
 thr.start()
